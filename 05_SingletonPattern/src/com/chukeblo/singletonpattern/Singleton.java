ecb66aa56c987b30bb95fb606742760fc428bf42
chuke <ken.0416.leo@gmail.com>
2022-02-27 01:59:18 +0900

    add singleton pattern

---
+++ 05_SingletonPattern/src/com/chukeblo/singletonpattern/Singleton.java
@@ -0,0 +1,13 @@
+package com.chukeblo.singletonpattern;
+
+public class Singleton {
+    private static Singleton singleton = new Singleton();
+
+    private Singleton() {
+        System.out.println("created a Singleton instance");
+    }
+
+    public static Singleton getInstance() {
+        return singleton;
+    }
+}
