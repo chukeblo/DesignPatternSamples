96623e69175bfc555a4621e84c6a58ec7d3cfa46
chuke <ken.0416.leo@gmail.com>
2022-02-27 02:29:12 +0900

    add prototype pattern

---
+++ 06_PrototypePattern/src/com/chukeblo/prototypepattern/framework/Product.java
@@ -0,0 +1,6 @@
+package com.chukeblo.prototypepattern.framework;
+
+public interface Product extends Cloneable {
+    public abstract void use(String s);
+    public abstract Product createCopy();
+}
